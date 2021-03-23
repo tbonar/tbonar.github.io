cf0b6cb12ac50ac6f8cc2e4d35ade28cc128dcc3
tbonar <32309887+tbonar@users.noreply.github.com>
2021-03-22 23:12:49 -0500

    Added a default beers option if a user does not have a file to input

--- CraftBeersApp/app.R
+++ CraftBeersApp/app.R
@@ -21,6 +21,7 @@ ui <- fluidPage(
        sidebarPanel(
          helpText(p("Use the following form to upload a CSV to input the data for app to analyze.")),
          fileInput("beersInput", "Beers", multiple = F, accept = c('text/csv','text/comma-separated-values,text/plain','.csv')),
+         checkboxInput("useDefaultBeers","Use a provided Beers.csv?", value = F),
          strong("Note: The beers dataset will be joined with a brewery dataset using a shared Brewery_id and the joined dataframe will be displayed on this tabs")
          ),
        # Outputs contained in navigation bar
@@ -38,14 +39,22 @@ server <- function(input, output) {
   output$beersDf <- renderDataTable(
     {
       inputFile <- input$beersInput
-      if(is.null(inputFile)) return(NULL)
+      # Don't store anything if input file is null and useDefaultBeers is false
+      if(is.null(inputFile) & !input$useDefaultBeers) return(NULL)
+      # Use app defaults if a user does not have a Beers dataset to use and a file was not provided
+      else if(input$useDefaultBeers & is.null(inputFile)) {
+        beers = read.csv("data/Beers.csv")
+      }
+      # Lastly if a file is provided, read and transform to a dataframe
+      else {
       beers <- read.csv(inputFile$datapath)
+      }
       
       beerAndBreweries = merge(beers, breweriesDf, by.x="Brewery_id", by.y="Brew_ID")
       colnames(beerAndBreweries)[2] = "Beer_Name"
       colnames(beerAndBreweries)[8] = "Brewery_Name"
       
-      beerAndBreweries
+      return(beerAndBreweries)
     })
 }
 
